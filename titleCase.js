198bb717fa722d7accf1058c30ea58632c3fe68d
Leah <rhodycodes401@gmail.com>
2018-05-25 02:25:45 +0000

    check

--- titleCase.js
+++ titleCase.js
@@ -1,28 +1,8 @@
 function titleCase(str) {
-  //var lowStr = str.toLowerCase();
-  var withCaps = "";
-  withCaps = str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
-    //console.log(withCaps);
-   for (var i = 1; i < str.length; i++){
-      if (str[i] == " "){
-          withCaps[i] = str[i + 1].toUpperCase();
-          
-               // console.log("Space at " + i);
-                // withCaps += withCaps.concat(str[i]);
-                // withCaps += withCaps.concat(str[i + 1]);
-                // i += 1;
-                // console.log(withCaps);
-                // //withCaps += withCaps.charAt(i + 1).toUpperCase();
-                // //i += 1;
- //            chars = withCaps.charAt(i).toUpperCase().concat(lowStr[i]);
-//           console.log("hello");
+  var withCaps = str.toLowerCase().split(' ');
+   for (var i = 0; i < withCaps.length; i++){
+      withCaps[i] = withCaps[i].charAt(0).toUpperCase() + withCaps[i].slice(1);
        }
-       //console.log(withCaps);
-        
-//   }
-   }
-    console.log(withCaps);
+    return withCaps.join(' ');
 }
-
-
 titleCase("I'm a little tea pot");
