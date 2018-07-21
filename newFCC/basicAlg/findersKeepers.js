0e11bb694e9833155465aa7c90254b0f31eab8fc
Leah <rhodycodes401@gmail.com>
2018-07-21 00:26:11 +0000

    revised

--- newFCC/basicAlg/findersKeepers.js
+++ newFCC/basicAlg/findersKeepers.js
@@ -22,3 +22,4 @@ return undefined;
 findElement([1, 2, 3, 4], num => num % 2 === 0);
 findElement([1, 3, 5, 8, 9, 10], function(num) { return num % 2 === 0; });
 findElement([1, 3, 5, 9], function(num) { return num % 2 === 0; });
+findElement([3, 5, 6, 4, 5, 10], function(num) { return num % 2 === 0; });
